dc9d517f2e48539888f422a28610c954e6faa01a
MohammadToumouk <129755740+MohammadToumouk@users.noreply.github.com>
2023-07-16 18:31:05 +0200

    fixed middleware naming

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,9 +19,6 @@ const authMiddleware = (requiredRoles) => {
       if (!requiredRoles.includes(decoded.user.role)) {
         return res.status(403).json({ message: 'Access denied. you have no permission for this page :/' });
       }
-      
-      // Attach the user object to the request for further processing
-      
       next();
     } catch (error) {
       return res.status(401).json({ message: 'Invalid token.' });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
-const authentication = require('../middleware/authMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 
 // User registration
 userRouter.post('/register', userController.registerUser);
@@ -10,13 +10,13 @@ userRouter.post('/register', userController.registerUser);
 userRouter.post('/login', userController.loginUser);
 
 // Get user profile
-userRouter.get('/profile',authentication(['admin','manger','employee']), userController.getUserProfile);
+userRouter.get('/profile',authMiddleware(['admin','manger','employee']), userController.getUserProfile);
 
 // Update user profile
-userRouter.put('/profile',authentication(['admin','manger']), userController.updateUserProfile);
+userRouter.put('/profile',authMiddleware(['admin','manger']), userController.updateUserProfile);
 
 // Get all users
-userRouter.get('/',authentication(['admin']), userController.getAllUsers);
+userRouter.get('/',authMiddleware(['admin']), userController.getAllUsers);
 
 // Logout user
 userRouter.post('/logout', userController.logoutUser);

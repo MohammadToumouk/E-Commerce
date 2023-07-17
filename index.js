db0f602175b02771b9e0df2e8c1aa57d4deb57ee
Oldschoolove <e.g.89@hotmail.de>
2023-07-17 17:28:22 +0200

    bugfix of server listner from / to :

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const cors = require('cors');
-const userRouter = require('./Routes/userRoutes');
-const productRouter = require('./routes/productRoutes');
-const orderRouter = require('./routes/orderRoutes');
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./Routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const orderRouter = require("./routes/orderRoutes");
 const port = 3069;
-require('dotenv/config');
-require('./db')
+require("dotenv/config");
+require("./db");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/user", userRouter)
-app.use("/product", productRouter)
-app.use("/order", orderRouter)
-
-
-
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/order", orderRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on Port: http://localhost/${port}`)
-})
+  console.log(`Server is running on Port: http://localhost:${port}`);
+});

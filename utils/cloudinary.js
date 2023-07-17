749450b2595d1773ed024c9168d6401db797979d
MohammadToumouk <129755740+MohammadToumouk@users.noreply.github.com>
2023-07-17 14:43:35 +0200

    initialzed Multer and added Image attribiut  to products

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dw7ny1wqs",
+  api_key: "713235996251383",
+  api_secret: "8qam0U6nyHSF530GD_CDmjPuCPI",
+});
+
+module.exports = cloudinary;

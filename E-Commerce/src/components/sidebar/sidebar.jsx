f8cce0d591554b5e426391928b3449b4f3437092
skale05 <skale05@yahoo.com>
2023-07-26 18:01:30 +0200

    sidebar

--- E-Commerce/src/components/sidebar/sidebar.jsx
+++ E-Commerce/src/components/sidebar/sidebar.jsx
@@ -1,159 +1,181 @@
-import React, { useState } from 'react';
-import './sidebar.css';
+import React, { useState, useEffect } from 'react';
+import './Sidebar.css';
 
-const Sidebar = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-  
-
-    const handleToggleSidebar = () => {
-        setSidebarOpen(!sidebarOpen);
-      };
-
+const Sidebar = ({ setFilteredProducts, products }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedBrands, setSelectedBrands] = useState([]);
   const [selectedNames, setSelectedNames] = useState([]);
   const [selectedSizes, setSelectedSizes] = useState([]);
   const [selectedColors, setSelectedColors] = useState([]);
   const [priceRange, setPriceRange] = useState([0, 5000]);
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [names, setNames] = useState([]);
+  const [sizes, setSizes] = useState([]);
+  const [colors, setColors] = useState([]);
 
- 
-  const categories = ['Mobile Phones', 'Tablets',];
-  const brands = ['Apple', 'Samsung', 'Huawei',];
-  const names = ['iPhone 13', 'iPhone 14', 'Samsung S23',];
-  const sizes = ['S', 'M', 'L', 'XL', 'XXL', ];
-  const colors = ['Blue', 'Green', 'Black', 'Purple', 'Orange',];
+  // Fetch data from the API and set filter options
+  useEffect(() => {
+    if (products.length > 0) {
+      setCategories([...new Set(products.map((product) => product.category))]);
 
-  const handleCategoryChange = (category) => {
-    setSelectedCategories((prevSelected) =>
-      prevSelected.includes(category)
-        ? prevSelected.filter((item) => item !== category)
-        : [...prevSelected, category]
-    );
-  };
+      const uniqueBrands = [...new Set(products.map((product) => product.brand))].filter(Boolean);
+      setBrands(uniqueBrands);
 
-  const handleBrandChange = (brand) => {
-    setSelectedBrands((prevSelected) =>
-      prevSelected.includes(brand)
-        ? prevSelected.filter((item) => item !== brand)
-        : [...prevSelected, brand]
-    );
-  };
+      const uniqueNames = [...new Set(products.map((product) => product.name))].filter(Boolean);
+      setNames(uniqueNames);
 
-  const handleNameChange = (name) => {
-    setSelectedNames((prevSelected) =>
-      prevSelected.includes(name)
-        ? prevSelected.filter((item) => item !== name)
-        : [...prevSelected, name]
-    );
-  };
+      const uniqueSizes = [...new Set(products.flatMap((product) => product.size))].filter(Boolean);
+      setSizes(uniqueSizes);
 
-  const handleSizeChange = (size) => {
-    setSelectedSizes((prevSelected) =>
-      prevSelected.includes(size)
-        ? prevSelected.filter((item) => item !== size)
-        : [...prevSelected, size]
-    );
-  };
+      const uniqueColors = [...new Set(products.flatMap((product) => product.color))].filter(Boolean);
+      setColors(uniqueColors);
 
-  const handleColorChange = (color) => {
-    setSelectedColors((prevSelected) =>
-      prevSelected.includes(color)
-        ? prevSelected.filter((item) => item !== color)
-        : [...prevSelected, color]
-    );
-  };
+      // Adjust price range based on the min and max values in the API
+      const minPrice = Math.min(...products.map((product) => product.price));
+      const maxPrice = Math.max(...products.map((product) => product.price));
+      setPriceRange([minPrice, maxPrice]);
+    }
+  }, [products]);
 
-  const handlePriceChange = (newValue) => {
-    setPriceRange(newValue);
+  // Filter logic
+  useEffect(() => {
+    const filteredProducts = products.filter((product) => {
+      const isCategoryMatch = selectedCategories.length === 0 || selectedCategories.includes(product.category);
+      const isBrandMatch = selectedBrands.length === 0 || selectedBrands.includes(product.brand);
+      const isNameMatch = selectedNames.length === 0 || selectedNames.includes(product.name);
+      const isSizeMatch = selectedSizes.length === 0 || selectedSizes.some((size) => product.size.includes(size));
+      const isColorMatch = selectedColors.length === 0 || selectedColors.some((color) => product.color.includes(color));
+      const isPriceMatch = product.price >= priceRange[0] && product.price <= priceRange[1];
+
+      return isCategoryMatch && isBrandMatch && isNameMatch && isSizeMatch && isColorMatch && isPriceMatch;
+    });
+
+    setFilteredProducts(filteredProducts);
+  }, [products, selectedCategories, selectedBrands, selectedNames, selectedSizes, selectedColors, priceRange, setFilteredProducts]);
+
+  const handleToggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
   };
 
   return (
     <div className={`sidebar ${sidebarOpen ? 'active' : ''}`}>
-      <button className="sidebar-toggle" onClick={handleToggleSidebar}>
-        ☰
-      </button>
+      {categories.length > 0 && (
+        <div>
+          <h3>Categories</h3>
+          {categories.map((category, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={selectedCategories.includes(category)}
+                onChange={() =>
+                  setSelectedCategories((prevCategories) =>
+                    prevCategories.includes(category)
+                      ? prevCategories.filter((cat) => cat !== category)
+                      : [...prevCategories, category]
+                  )
+                }
+              />
+              {category}
+            </label>
+          ))}
+        </div>
+      )}
 
-      <div>
-        <h3>Categories</h3>
-        {categories.map((category) => (
-          <label key={category}>
-            <input
-              type="checkbox"
-              checked={selectedCategories.includes(category)}
-              onChange={() => handleCategoryChange(category)}
-            />
-            {category}
-          </label>
-        ))}
-      </div>
+      {brands.length > 0 && (
+        <div>
+          <h3>Brands</h3>
+          {brands.map((brand, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={selectedBrands.includes(brand)}
+                onChange={() =>
+                  setSelectedBrands((prevBrands) =>
+                    prevBrands.includes(brand) ? prevBrands.filter((b) => b !== brand) : [...prevBrands, brand]
+                  )
+                }
+              />
+              {brand}
+            </label>
+          ))}
+        </div>
+      )}
 
-      <div>
-        <h3>Brands</h3>
-        {brands.map((brand) => (
-          <label key={brand}>
-            <input
-              type="checkbox"
-              checked={selectedBrands.includes(brand)}
-              onChange={() => handleBrandChange(brand)}
-            />
-            {brand}
-          </label>
-        ))}
-      </div>
+      {names.length > 0 && (
+        <div>
+          <h3>Names</h3>
+          {names.map((name, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={selectedNames.includes(name)}
+                onChange={() =>
+                  setSelectedNames((prevNames) =>
+                    prevNames.includes(name) ? prevNames.filter((n) => n !== name) : [...prevNames, name]
+                  )
+                }
+              />
+              {name}
+            </label>
+          ))}
+        </div>
+      )}
 
-      <div>
-        <h3>Names</h3>
-        {names.map((name) => (
-          <label key={name}>
-            <input
-              type="checkbox"
-              checked={selectedNames.includes(name)}
-              onChange={() => handleNameChange(name)}
-            />
-            {name}
-          </label>
-        ))}
-      </div>
+      {sizes.length > 0 && (
+        <div>
+          <h3>Sizes</h3>
+          {sizes.map((size, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={selectedSizes.includes(size)}
+                onChange={() =>
+                  setSelectedSizes((prevSizes) =>
+                    prevSizes.includes(size) ? prevSizes.filter((s) => s !== size) : [...prevSizes, size]
+                  )
+                }
+              />
+              {size}
+            </label>
+          ))}
+        </div>
+      )}
 
-      <div>
-        <h3>Sizes</h3>
-        {sizes.map((size) => (
-          <label key={size}>
-            <input
-              type="checkbox"
-              checked={selectedSizes.includes(size)}
-              onChange={() => handleSizeChange(size)}
-            />
-            {size}
-          </label>
-        ))}
-      </div>
+      {colors.length > 0 && (
+        <div>
+          <h3>Colors</h3>
+          {colors.map((color, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                checked={selectedColors.includes(color)}
+                onChange={() =>
+                  setSelectedColors((prevColors) =>
+                    prevColors.includes(color) ? prevColors.filter((c) => c !== color) : [...prevColors, color]
+                  )
+                }
+              />
+              {color}
+            </label>
+          ))}
+        </div>
+      )}
 
-      <div>
-        <h3>Colors</h3>
-        {colors.map((color) => (
-          <label key={color}>
-            <input
-              type="checkbox"
-              checked={selectedColors.includes(color)}
-              onChange={() => handleColorChange(color)}
-            />
-            {color}
-          </label>
-        ))}
-      </div>
-
-      <div>
-        <h3>Price Range</h3>
-        <input
-          type="range"
-          value={priceRange[1]}
-          onChange={(e) => handlePriceChange([priceRange[0], e.target.value])}
-          min={0}
-          max={5000}
-        />
-        {priceRange[0]} - {priceRange[1]}
-      </div>
+      {priceRange.length > 0 && (
+        <div>
+          <h3>Price Range</h3>
+          <input
+            type="range"
+            min={0}
+            max={5000}
+            value={priceRange[0]}
+            onChange={(e) => setPriceRange([parseInt(e.target.value), priceRange[1]])}
+          />
+          <span>{`€${priceRange[0]} - €${priceRange[1]}`}</span>
+        </div>
+      )}
     </div>
   );
 };
--- E-Commerce/src/pages/ShopPage/ShopPage.jsx
+++ E-Commerce/src/pages/ShopPage/ShopPage.jsx
@@ -1,67 +1,40 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
+// ShopPage.jsx
+import React, { useState, useEffect } from 'react';
 import './ShopPage.css';
-import Card from '../../components/card/card';
 import Sidebar from '../../components/sidebar/sidebar';
-
-
-
-
-
-const cardData = [
-  {
-    id: 1,
-    color: 'red',
-    available: true,
-    imageSrc: 'https://www.citypng.com/public/uploads/preview/iphone-14-pro-and-max-deep-purple-png-11662587434zacaxkb4sd.png',
-    productName: 'Iphone 13',
-    price: '199.99',
-  },
-  {
-    id: 2,
-    color: 'blue',
-    available: false,
-    imageSrc: 'https://www.citypng.com/public/uploads/preview/iphone-14-pro-and-max-deep-purple-png-11662587434zacaxkb4sd.png',
-    productName: 'Iphone 13',
-    price: '499.99',
-  },
-  {
-    id: 3,
-    color: 'black',
-    available: true,
-    imageSrc: 'https://www.citypng.com/public/uploads/preview/iphone-14-pro-and-max-deep-purple-png-11662587434zacaxkb4sd.png',
-    productName: 'Iphone 13',
-    price: '499.99',
-  },
-  {
-    id: 4,
-    color: 'purple',
-    available: true,
-    imageSrc: 'https://www.citypng.com/public/uploads/preview/iphone-14-pro-and-max-deep-purple-png-11662587434zacaxkb4sd.png',
-    productName: 'Iphone 13',
-    price: '499.99',
-  },
-];
+import Card from '../../components/Card/Card';
 
 const ShopPage = () => {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  // Fetch data from the API
+  useEffect(() => {
+    fetch('http://localhost:3069/product/')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="shop-page">
-      {/* Button to toggle the sidebar */}
-      <button className="sidebar-toggle" onClick={() => window.dispatchEvent(new Event('toggleSidebar'))}>
-        ☰ Toggle Sidebar
-      </button>
+      <Sidebar setFilteredProducts={setFilteredProducts} products={products} />
 
       <div className="card-container">
-        {cardData.map((card) => (
+        {filteredProducts.map((product) => (
           <Card
-            key={card.id}
-            available={card.available}
-            imageSrc={card.imageSrc}
-            productName={card.productName}
-            price={card.price}
-            color={card.color}
+            key={product._id}
+            available={product.quantity > 0}
+            imageSrc={product.images[0]} // Use the first image URL from the 'images' array if available
+            productName={product.name}
+            price={product.price}
+            color="black"
             currency="€"
-            productId={card.id}
+            productId={product._id}
           />
         ))}
       </div>

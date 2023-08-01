5bf7110d188de3700daf88c250958bb10f2f7616
skale05 <skale05@yahoo.com>
2023-08-01 11:09:20 +0200

    minor styling

--- E-Commerce/src/components/ContactUs/ContactUsSection.jsx
+++ E-Commerce/src/components/ContactUs/ContactUsSection.jsx
@@ -13,7 +13,7 @@ export const ContactUsSection = () => {
     <div ref={ref} className={`fade-in ${inView ? 'visible' : ''}`}>
     <div className="flex flex-row items-center justify-between h-80 mx-20">
     <p className="text-2xl ">Need Help ?&#8594;</p>
-    <h1 className="text-8xl flex-1 w-64 align-middle underline decoration-yellow-200 hover:cursor-pointer">Contact us</h1>
+    <a href="/contact" className="text-8xl flex-1 w-64 align-middle underline decoration-yellow-200 hover:cursor-pointer">Contact us</a>
     <div className="bg-yellow-300 w-20 h-20 rounded-full flex items-center justify-center">
       <p style={{ fontSize: "30px" }}>&#8594;</p>
     </div>

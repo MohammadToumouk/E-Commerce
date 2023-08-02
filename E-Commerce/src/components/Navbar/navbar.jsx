9f6a99b94ffd7515522687cce6116e6713603981
Oldschoolove <e.g.89@hotmail.de>
2023-08-02 22:55:50 +0200

    bug fix of localhost

--- E-Commerce/src/components/Navbar/navbar.jsx
+++ E-Commerce/src/components/Navbar/navbar.jsx
@@ -145,7 +145,7 @@ const Navbar = ({customer, shoppingList, setShoppingList}) => {
     
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="http://localhost:5174" className="flex items-center">
+    <a href={(baseUrl || "")} className="flex items-center">
         <img src="https://i.ibb.co/2h36knH/logo.jpg" className="h-16 mr-3" alt="Emazing Logo" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
     </a>
--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-const { addItemToCart, getCustomerCart } = require("../controllers/cartController"); // Modify the path to your shopping cart code
+const {
+  addItemToCart,
+  getCustomerCart,
+} = require("../controllers/cartController"); // Modify the path to your shopping cart code
 
 stripeRouter.use(express.static("public"));
 stripeRouter.use(bodyParser.json());
@@ -12,14 +15,14 @@ stripeRouter.use(bodyParser.json());
 async function createStripePrice(productID, price, currency) {
   try {
     const priceObj = await stripe.prices.create({
-      unit_amount: price*100, // Provide the price amount in the smallest currency unit (e.g., cents for USD)
+      unit_amount: price * 100, // Provide the price amount in the smallest currency unit (e.g., cents for USD)
       currency: currency, // The currency code (e.g., 'usd')
       product: productID, // Use the Product ID obtained from the createStripeProduct function
       // You can add more options to the Price object if needed
     });
     return priceObj.id; // Return the ID of the created Price object
   } catch (error) {
-    console.error('Error creating Price object:', error);
+    console.error("Error creating Price object:", error);
     throw error;
   }
 }
@@ -28,76 +31,69 @@ async function createStripeProduct(name, description) {
   try {
     const product = await stripe.products.create({
       name: name,
-      description:description,
+      description: description,
       // You can add more options to the Product object if needed
     });
     return product.id; // Return the ID of the created Product object
   } catch (error) {
-    console.error('Error creating Product object:', error);
+    console.error("Error creating Product object:", error);
     throw error;
   }
 }
 
 stripeRouter.post(
-    "/checkout/create-checkout-session",
-    async (req, res, next) => {
-      try {
-        // Get the customer's cart items and their quantities
-    const cartItems = req.body.shoppingCart?.cart?.items;
+  "/checkout/create-checkout-session",
+  async (req, res, next) => {
+    try {
+      // Get the customer's cart items and their quantities
+      const cartItems = req.body.shoppingCart?.cart?.items;
 
-    // Create an array of line items for Stripe Checkout
-    const lineItems = [];
-    for (const item of cartItems) {
-      const productID = await createStripeProduct(item.name);
-      const priceID = await createStripePrice(productID, item.price, 'usd');
-      lineItems.push({
-        price: priceID,
-        quantity: item.quantity,
-      });
-    }
-
-    if (lineItems.length === 0) {
-      // Handle empty cart here (send an error response or redirect to cart page)
-      return res.status(400).json({ error: "The shopping cart is empty." });
-      
-    }
-    
-  
-        // Create the Stripe Checkout session with the cart items
-        const session = await stripe.checkout.sessions.create({
-          billing_address_collection: "auto",
-          shipping_address_collection: {
-            allowed_countries: ["DE", "US"],
-          },
-          line_items : lineItems,
-          
-          
-          mode: "payment",
-          success_url: `${"http://localhost:5174/successpayment"}?success=true`,
-          cancel_url: `${"http://localhost:5174/failed"}?canceled=true`,
+      // Create an array of line items for Stripe Checkout
+      const lineItems = [];
+      for (const item of cartItems) {
+        const productID = await createStripeProduct(item.name);
+        const priceID = await createStripePrice(productID, item.price, "usd");
+        lineItems.push({
+          price: priceID,
+          quantity: item.quantity,
         });
-       res.json({url: session.url}) 
-       // res.redirect( session.url);
-        console.log(session.url)
-      } catch (error) {
-        console.log(error)
-        // Pass the error to the error handling middleware
-        next(error);
-        
+      }
+
+      if (lineItems.length === 0) {
+        // Handle empty cart here (send an error response or redirect to cart page)
+        return res.status(400).json({ error: "The shopping cart is empty." });
+      }
+
+      // Create the Stripe Checkout session with the cart items
+      const session = await stripe.checkout.sessions.create({
+        billing_address_collection: "auto",
+        shipping_address_collection: {
+          allowed_countries: ["DE", "US"],
+        },
+        line_items: lineItems,
+
+        mode: "payment",
+        success_url: `${(baseUrl || "") + "/successpayment"}?success=true`,
+        cancel_url: `${(baseUrl || "") + "/failed"}?canceled=true`,
+      });
+      res.json({ url: session.url });
+      // res.redirect( session.url);
+      console.log(session.url);
+    } catch (error) {
+      console.log(error);
+      // Pass the error to the error handling middleware
+      next(error);
+
       //  console.log(req.body.shoppingList.cart._id);
       //  console.log(req.body.shoppingCart.cart)
-   //   console.log(req.body.shoppingCart.cart?.items[0].quantity)
-        
-      
-      }
+      //   console.log(req.body.shoppingCart.cart?.items[0].quantity)
     }
-  );
-  
+  }
+);
 
 // Error handling middleware for general errors
 stripeRouter.use((error, req, res, next) => {
   res.status(500).json({ error: "Something went wrong." });
-  
 });
 
 // Error handling middleware for Stripe-related errors
@@ -116,7 +112,5 @@ stripeRouter.use((error, req, res, next) => {
     res.status(500).json({ error: "Something went wrong with the payment." });
   }
 });
-
-
 
 module.exports = stripeRouter;

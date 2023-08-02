28ed626efe91ddb481c93ccf72cdc3b99ba6283f
skale05 <skale05@yahoo.com>
2023-08-02 18:14:02 +0200

    thank you message

--- E-Commerce/src/pages/PaymentPages/SuccessPayment.jsx
+++ E-Commerce/src/pages/PaymentPages/SuccessPayment.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export const SuccessPayment = () => {
+  const buttonStyle = {
+    padding: '10px 20px',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    borderRadius: '4px',
+    textDecoration: 'none',
+    display: 'inline-block',
+    marginTop: '20px',
+  };
+
   return (
-    <div>Thx for Shopping Here</div>
-  )
-}
+    <div>
+      <h2 style={{ fontSize: '24px' }}>Thank you for shopping at our EMAzing Store</h2>
+      <a href="/shop" style={buttonStyle}>Go to Shop</a>
+    </div>
+  );
+};

bef8f74cf25085ae7a736ec1c1cbb3d2a913660e
skale05 <skale05@yahoo.com>
2023-08-02 13:09:05 +0200

    profile page

--- E-Commerce/src/pages/MyProfile/myprofile.jsx
+++ E-Commerce/src/pages/MyProfile/myprofile.jsx
@@ -1,196 +1,245 @@
-import React, { useState} from 'react';
-import './Myprofile.css'
-
-
-const initialProfileItems = [
-  { label: 'Name', value: 'John', isEditing: false },
-  { label: 'Last Name', value: 'Doe', isEditing: false },
-  { label: 'Email', value: 'john.doe@example.com', isEditing: false },
-  { label: 'Phone', value: '123-456-7890', isEditing: false },
-  {
-    label: 'Address',
-    value: { street: '123 Main Street', city: 'Detroit', state: 'USA', postalCode: '' },
-    isEditing: false,
-  },
-];
+import React, { useState, useEffect } from 'react';
+import './Myprofile.css';
 
 const formatAddress = (address) => {
   const { street, city, state, postalCode } = address;
   return `${street}, ${city}, ${state}, ${postalCode}`;
 };
 
-const MyProfile = ({setCustomer, customer}) => {
-  const [profileItems, setProfileItems] = useState(initialProfileItems);
-  const [firstname, setFirstname] = useState(customer?.customer?.firstname)
-  const [lastname, setLastname] = useState(customer?.customer?.lastname)
-  const [email, setEmail] = useState(customer?.customer?.email)
-  const [phone, setPhone] = useState(customer?.customer?.phone)
-  const [address, setAddress] = useState(customer?.customer?.address)
-  const [city, setCity] = useState(customer?.customer?.city)
-  const [state, setState] = useState(customer?.customer?.state)
-  const [postalCode, setPostalCode] = useState(customer?.customer?.postalCode)
-  const [isEditing, setIsEditing] = useState(false)
+const MyProfile = ({ setCustomer, customer }) => {
+  const [isEditing, setIsEditing] = useState({
+    name: false,
+    lastName: false,
+    email: false,
+    phone: false,
+    address: false,
+  });
 
-  
-  console.log("customer:", customer)
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState({
+    street: '',
+    city: '',
+    state: '',
+    postalCode: '',
+  });
 
-  const handleEditClick = (index) => {
-    setProfileItems((prevItems) => {
-      const updatedItems = [...prevItems];
-      updatedItems[index].isEditing = true;
-      return updatedItems;
-    });
+  const handleAddressChange = (e) => {
+    const { name, value } = e.target;
+    setAddress((prevAddress) => ({
+      ...prevAddress,
+      [name]: value,
+    }));
   };
 
-  const handleSaveClick = (index) => {
-    setProfileItems((prevItems) => {
-      const updatedItems = [...prevItems];
-      updatedItems[index].isEditing = false;
-      return updatedItems;
+  // Fetch customer data from the database when the component mounts or when the customer prop changes
+  useEffect(() => {
+    setFirstname(customer?.customer?.firstname || '');
+    setLastname(customer?.customer?.lastname || '');
+    setEmail(customer?.customer?.email || '');
+    setPhone(customer?.customer?.phone || '');
+    setAddress(customer?.customer?.address || {
+      street: '',
+      city: '',
+      state: '',
+      postalCode: '',
     });
+  }, [customer]);
+
+  const handleEditClick = (section) => {
+    setIsEditing((prevIsEditing) => ({
+      ...prevIsEditing,
+      [section]: true,
+    }));
   };
 
+  const handleSaveClick = (section) => {
+    setIsEditing((prevIsEditing) => ({
+      ...prevIsEditing,
+      [section]: false,
+    }));
 
-{/* <---emi-start---> */}
+    if (section === 'name' || section === 'lastName') {
+      setCustomer((prevCustomer) => ({
+        ...prevCustomer,
+        customer: {
+          ...prevCustomer.customer,
+          firstname,
+          lastname,
+        },
+      }));
+    } else if (section === 'email') {
+      setCustomer((prevCustomer) => ({
+        ...prevCustomer,
+        customer: {
+          ...prevCustomer.customer,
+          email,
+        },
+      }));
+    } else if (section === 'phone') {
+      setCustomer((prevCustomer) => ({
+        ...prevCustomer,
+        customer: {
+          ...prevCustomer.customer,
+          phone,
+        },
+      }));
+    } else if (section === 'address') {
+      setCustomer((prevCustomer) => ({
+        ...prevCustomer,
+        customer: {
+          ...prevCustomer.customer,
+          address,
+        },
+      }));
+    }
+  };
+
   return (
     <div className="profile-container">
-      <h1>My Profile</h1>
+      <h1 className="profile-header">My Profile</h1>
       <div className="profile-info">
         <div className="profile-item">
-          <label>Name:</label>
-          <div>
-          <input
-              type="text"
-              value={firstname}
-              placeholder="First Name"
-              onChange={(e) => { setFirstname(e.target.value) }}
-              className="profile-input"
-            />
-          </div>
-        </div>
-        <div className="profile-item">
-          <label>Last Name:</label>
-          <div>
-          <input
-              type="text"
-              value={lastname}
-              placeholder="Last Name"
-              onChange={(e) => { setLastname(e.target.value) }}
-              className="profile-input"
-            />
-          </div>
-        </div>
-        <div className="profile-item">
-          <label>E-Mail:</label>
-          <div>
-          <input
-              type="text"
-              value={email}
-              placeholder="E-Mail"
-              onChange={(e) => { setEmail(e.target.value) }}
-              className="profile-input"
-            />
-          </div>
-        </div>
-        <div className="profile-item">
-          <label>Phone:</label>
-          <div>
-          <input
-              type="text"
-              value={phone}
-              placeholder="Phone Number"
-              onChange={(e) => { setPhone(e.target.value) }}
-              className="profile-input"
-            />
-          </div>
-        </div>
-{/* <---emi-end---> */}
-
-        {profileItems.map((item, index) => (
-          <div className="profile-item" key={index}>
-            <label>{item.label}:</label>
-            {item.isEditing ? (
-              item.label === 'Address' ? (
-                <div>
-                  <input
-                    type="text"
-                    value={item.value.street}
-                    onChange={(e) => {
-                      const newValue = e.target.value;
-                      setProfileItems((prevItems) => {
-                        const updatedItems = [...prevItems];
-                        updatedItems[index].value.street = newValue;
-                        return updatedItems;
-                      });
-                    }}
-                    placeholder="Enter your Street"
-                  />
-                  <input
-                    type="text"
-                    value={item.value.city}
-                    onChange={(e) => {
-                      const newValue = e.target.value;
-                      setProfileItems((prevItems) => {
-                        const updatedItems = [...prevItems];
-                        updatedItems[index].value.city = newValue;
-                        return updatedItems;
-                      });
-                    }}
-                    placeholder="Enter your City"
-                  />
-                  <input
-                    type="text"
-                    value={item.value.state}
-                    onChange={(e) => {
-                      const newValue = e.target.value;
-                      setProfileItems((prevItems) => {
-                        const updatedItems = [...prevItems];
-                        updatedItems[index].value.state = newValue;
-                        return updatedItems;
-                      });
-                    }}
-                    placeholder="Enter your State"
-                  />
-                  <input
-                    type="text"
-                    value={item.value.postalCode}
-                    onChange={(e) => {
-                      const newValue = e.target.value;
-                      setProfileItems((prevItems) => {
-                        const updatedItems = [...prevItems];
-                        updatedItems[index].value.postalCode = newValue;
-                        return updatedItems;
-                      });
-                    }}
-                    placeholder="Enter your postal code"
-                  />
-                </div>
-              ) : (
+          <div className="profile-label">Name:</div>
+          <div className="profile-data">
+            {isEditing.name ? (
+              <>
                 <input
                   type="text"
-                  value={item.value}
-                  onChange={(e) => {
-                    const newValue = e.target.value;
-                    setProfileItems((prevItems) => {
-                      const updatedItems = [...prevItems];
-                      updatedItems[index].value = newValue;
-                      return updatedItems;
-                    });
-                  }}
+                  value={firstname}
+                  placeholder="First Name"
+                  onChange={(e) => setFirstname(e.target.value)}
+                  className="profile-input"
                 />
-              )
+                <button onClick={() => handleSaveClick('name')} className="profile-button">Save</button>
+              </>
             ) : (
-              <span>
-                {item.label === 'Address' ? formatAddress(item.value) : item.value}
-              </span>
-            )}
-            {item.isEditing ? (
-              <button onClick={() => handleSaveClick(index)}>Save</button>
-            ) : (
-              <button onClick={() => handleEditClick(index)}>🖉</button>
+              <>
+                <div>{firstname}</div>
+                <button onClick={() => handleEditClick('name')} className="profile-button">Edit</button>
+              </>
             )}
           </div>
-        ))}
+        </div>
+
+        <div className="profile-item">
+          <div className="profile-label">Last Name:</div>
+          <div className="profile-data">
+            {isEditing.lastName ? (
+              <>
+                <input
+                  type="text"
+                  value={lastname}
+                  placeholder="Last Name"
+                  onChange={(e) => setLastname(e.target.value)}
+                  className="profile-input"
+                />
+                <button onClick={() => handleSaveClick('lastName')} className="profile-button">Save</button>
+              </>
+            ) : (
+              <>
+                <div>{lastname}</div>
+                <button onClick={() => handleEditClick('lastName')} className="profile-button">Edit</button>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="profile-item">
+          <div className="profile-label">E-Mail:</div>
+          <div className="profile-data">
+            {isEditing.email ? (
+              <>
+                <input
+                  type="text"
+                  value={email}
+                  placeholder="E-Mail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="profile-input"
+                />
+                <button onClick={() => handleSaveClick('email')} className="profile-button">Save</button>
+              </>
+            ) : (
+              <>
+                <div>{email}</div>
+                <button onClick={() => handleEditClick('email')} className="profile-button">Edit</button>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="profile-item">
+          <div className="profile-label">Phone:</div>
+          <div className="profile-data">
+            {isEditing.phone ? (
+              <>
+                <input
+                  type="text"
+                  value={phone}
+                  placeholder="Phone Number"
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="profile-input"
+                />
+                <button onClick={() => handleSaveClick('phone')} className="profile-button">Save</button>
+              </>
+            ) : (
+              <>
+                <div>{phone}</div>
+                <button onClick={() => handleEditClick('phone')} className="profile-button">Edit</button>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="profile-item">
+          <div className="profile-label">Address:</div>
+          <div className="profile-data">
+            {isEditing.address ? (
+              <>
+                <input
+                  type="text"
+                  name="street"
+                  value={address.street}
+                  placeholder="Street"
+                  onChange={handleAddressChange}
+                  className="profile-input"
+                />
+                <input
+                  type="text"
+                  name="city"
+                  value={address.city}
+                  placeholder="City"
+                  onChange={handleAddressChange}
+                  className="profile-input"
+                />
+                <input
+                  type="text"
+                  name="state"
+                  value={address.state}
+                  placeholder="State"
+                  onChange={handleAddressChange}
+                  className="profile-input"
+                />
+                <input
+                  type="text"
+                  name="postalCode"
+                  value={address.postalCode}
+                  placeholder="Postal Code"
+                  onChange={handleAddressChange}
+                  className="profile-input"
+                />
+                <button onClick={() => handleSaveClick('address')} className="profile-button">Save</button>
+              </>
+            ) : (
+              <>
+                <div>{formatAddress(address)}</div>
+                <button onClick={() => handleEditClick('address')} className="profile-button">Edit</button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
